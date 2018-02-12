a8cfeadf7492f35636470e383bdb312411c93e98
Baghbahari <bahari@knights.ucf.edu>
2018-02-12 10:16:26 -0500

    Add files via upload

--- KNN_HW.py
+++ KNN_HW.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import time
 import sys,os
+import psutil
 from operator import itemgetter
 	#------ Read the train and test data set from txt file
 train = pd.read_csv('train.txt',header=None,delimiter=r"\s+")
@@ -14,30 +15,32 @@ else:
 	Testfile = 'validate.txt'
 
 validate = pd.read_csv(Testfile,header=None,delimiter=r"\s+")
-validate=np.array(validate[0::2])#traino[1::1]#
+validate=np.array(validate[0::1])#traino[1::1]#
 E = 0 
 AccK = [0]
 def dist(train,validate):
-	D2 = []
+	process = psutil.Process(os.getpid())
 	D2t = []
 	W=0.1*np.identity(784)
 	t0 = time.clock()
 	for i in range(0,validate.shape[0]):
-		V = validate[i][0:-1]*np.ones((train.shape[0],1))
-		D = (train[:,0:-1]-V)#np.dot((train[:,0:-1]-V),W)#
 		'''
+		D2 = []
 		for j in range(0,train.shape[0]):
 			D = train[j][0:-1] - validate[i][0:-1]
 			D = np.sqrt(np.sum(np.square(D)))
 			D2.append(D)
 		'''
+		V = validate[i][0:-1]*np.ones((train.shape[0],1))
+		D = (train[:,0:-1]-V)#np.dot((train[:,0:-1]-V),W)#
 		D2 = list(np.sqrt(np.sum(np.square(D),axis = 1)))
 		D2z = zip(list(train[:,-1]),D2)#print len(D2)
 		#print (D2z)
 		D2z.sort(key=lambda x: x[1])
 		D2t.append(D2z)
 		#Knn = D2z[0:K]
-	print time.clock()-t0
+	print(time.clock()-t0)
+	print(process.memory_info().rss)
 	return D2t
 
 def KNN_Acc(D2z,validate,K):
@@ -62,13 +65,13 @@ acc = KNN_Acc(train,validate,K)
 print acc
 '''
 D2z = dist(train,validate)
-for K in range(1,2*30,1): 
+for K in range(1,2*100,1): 
 	acc = KNN_Acc(D2z,validate,K)
 	AccK.append(acc)
 #print(AccK)
 import matplotlib.pyplot as plt
 ww = train[1400,0:-1].reshape(28,28)
-print ww.shape
+print(ww.shape)
 #plt.imshow(ww.T)
 plt.plot(AccK)
 plt.show()
